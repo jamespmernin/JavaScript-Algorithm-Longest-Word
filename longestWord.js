4a58de44652af15b3bc59a5bf8beba0c820b2ded
James Mernin <jamespmernin@gmail.com>
2020-10-06 17:14:31 -0400

    Algorithm solved

--- longestWord.js
+++ longestWord.js
@@ -1,7 +1,14 @@
 //LONGEST WORD
 //return the longest word in a given array
 const longestWord = str => {
- 
+  let strSplit = str.split(' ');
+  let longestWord = '';
+  for (let i = 0; i < strSplit.length; i++) {
+    if (strSplit[i].length > longestWord.length) {
+      longestWord = strSplit[i];
+    }
+  }
+  return longestWord;
 }
 console.log(longestWord("The quick brown fox jumped over the lazy dog"))
 
